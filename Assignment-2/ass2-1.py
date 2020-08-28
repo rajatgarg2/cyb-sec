5e62e5ce1fe1555bc342fe1b35199a189ab86617
Rajat Garg <68014249+rajatgarg2@users.noreply.github.com>
2020-08-28 20:11:06 +0530

    Delete ass2-1.py

--- Assignment-2/ass2-1.py
+++
@@ -1,8 +0,0 @@
-def check(l,h,n):
-    if n>=l and n<=h:
-        print('True')
-    else:
-        print('False')
-l,h=int(input('Enter the lower limit : ')),int(input('Enter the higher limit : '))
-n=int(input('Enter a number : '))
-check(l,h,n)
